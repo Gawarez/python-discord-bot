82783e31f57cf57955740590cda9f30ba7cfb8f0
Gawarez <gabrieltbaggio@gmail.com>
2024-02-21 07:58:56 -0300

    fix

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-from PythonBot.paramiko import runCommandSSH
+from paramiko import runCommandSSH
 
 intents = discord.Intents.default()
 intents.message_content = True
